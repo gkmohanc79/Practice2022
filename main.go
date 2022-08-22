d0f167900f16e9aec32bee42a5e67507e38b5f6f
krishna <gkmohanc79@gmail.com>
2022-08-22 16:50:54 -0500

    new file added

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x int = 10
+	var y int = 20
+
+	var res int = add(x, y)
+	var res2 int = sub(x, y)
+	var res3 int = multi(x, y)
+
+	fmt.Println(res)
+	fmt.Println(res2)
+	fmt.Println(res3)
+}
+
+func add(a int, b int) int {
+
+	var c int
+	c = a + b
+	return c
+
+}
+
+func sub(a int, b int) int {
+
+	var c int
+	c = a - b
+	return c
+
+}
+func multi(a int, b int) int {
+
+	var c int
+	c = a * b
+	return c
+
+}
