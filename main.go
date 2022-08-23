23ac833de9fd51e8270175230ab31bb73300d73e
krishna <gkmohanc79@gmail.com>
2022-08-22 21:27:47 -0500

    new update

--- main.go
+++ main.go
@@ -5,46 +5,6 @@ import "fmt"
 func main() {
 
 	var x int = 10
-	var y int = 20
 
-	var res int = add(x, y)
-	var res2 int = sub(x, y)
-	var res3 int = multi(x, y)
-	//var res4 int = div(x, y)
-
-	fmt.Println(res)
-	fmt.Println(res2)
-	fmt.Println(res3)
-	//fmt.Println(res4)
+	fmt.Println(x)
 }
-
-func add(a int, b int) int {
-
-	var c int
-	c = a + b
-	return c
-
-}
-
-func sub(a int, b int) int {
-
-	var c int
-	c = a - b
-	return c
-
-}
-func multi(a int, b int) int {
-
-	var c int
-	c = a * b
-	return c
-
-}
-
-// func div(a, b int) int {
-
-// 	var c int
-// 	c = a / b
-// 	return c
-
-// }
