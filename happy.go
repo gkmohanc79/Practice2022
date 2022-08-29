e9b4ea6dcf2e9e5337bd111119b1914ad6f3ac9a
krishna <gkmohanc79@gmail.com>
2022-08-29 16:09:19 -0500

    updated

--- happy.go
+++ happy.go
@@ -6,7 +6,24 @@ func main() {
 
 	var x = 10
 	var y = &x
-	var z = &y
+	//var z = &y
 	//fmt.Println(&x, *y, **z)
-	fmt.Println(x, &y, *z)
+	fmt.Println(y)
 }
+
+// func main() {
+
+// 	var i = 55
+// 	var j = 66.19
+
+// 	add := float(i) + j
+
+// 	fmt.Println("add")
+// }
+
+// func add(i int, j int) int {
+
+// 	var c int
+// 	c = i + j
+// 	return c
+// }
